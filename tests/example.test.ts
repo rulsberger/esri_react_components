22561618bbe13654d633fe2ef09daa9280f62602
Ryan.Ulsberger <ryan.ulsberger@piercecountywa.gov>
2025-02-18 19:04:26 -0800

    Remove the simple test

--- tests/example.test.ts
+++
@@ -1,9 +0,0 @@
-// filepath: /tests/example.test.ts
-import { describe, it, expect } from 'vitest';
-
-
-describe('sum', () => {
-  it('For True to be True', () => {
-    expect(true).toBe(true);
-  });
-});
