7ba19a7a23e32ff76343519c7b794e18d96490ec
Ryan.Ulsberger <ryan.ulsberger@piercecountywa.gov>
2025-02-27 16:16:13 -0800

    Clean Up utility to get sublayer from MapImageServer

--- src/libs/queryByGeometry.ts
+++ src/libs/queryByGeometry.ts
@@ -15,25 +15,38 @@ export default async function queryByGeometry(
 ): Promise<LayerQueryResults[]> {
   const resultsByLayer: LayerQueryResults[] = [];
 
-  async function traverseLayers(layer: __esri.MapImageLayer) {
-    const allLayers = layer.allSublayers.toArray();
+  async function traverseLayers(layer: __esri.Sublayer | __esri.MapImageLayer) {
+    const allLayers = layer.sublayers ? layer.sublayers.toArray() : [];
 
     for (const sublayer of allLayers) {
-      if (!sublayer.sublayers && sublayer.url && (!onlyVisible || sublayer.visible)) {
+      if (sublayer.sublayers && sublayer.sublayers.length > 0) {
+        // Recursively traverse sublayers
+        await traverseLayers(sublayer);
+      } else if (sublayer.url && (!onlyVisible || sublayer.visible)) {
+        console.log("Query Sublayer", sublayer.title);
+        console.log("Sublayer", sublayer);
+        // Query the sublayer if it has no sublayers
         const result = await QueryService.queryFeatureLayer(sublayer.url, geometry, sublayer.popupTemplate);
         if (result) {
-          console.log(result);
+          console.log("Query Results", result);
           resultsByLayer.push(result);
         }
       }
     }
   }
 
-  for (const layer of mapView.map.layers.toArray()) {
-    if (layer.type === "map-image") {
-      await traverseLayers(layer as __esri.MapImageLayer);
-    }
-  }
+  // loop through webmap's operational layers
+  mapView.map.layers.forEach((layer, index) => {
+    mapView
+      .whenLayerView(layer)
+      .then((layerView) => {
+        if (layer.type === "map-image") {
+          traverseLayers(layer as unknown as __esri.MapImageLayer);
+        }
+      })
+      .catch(console.error);
+  });
 
+  console.log(resultsByLayer)
   return resultsByLayer;
 }
